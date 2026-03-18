9a15adb400868c4bbb69b83bfaa7290c4a204833
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-03-18 10:37:47 -0300

    chore: remove arquivos desnecessarios

--- obj/Release/net8.0/Usuarios-API.AssemblyInfo.cs
+++ obj/Release/net8.0/Usuarios-API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios-API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d502d4ed92a08c4b865e5a0693441033da3a17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios-API")]
